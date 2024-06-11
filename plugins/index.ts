6847319bf01fe0482c05e02e4dbe02d36663cd5a
Stephanie Lin <102728558+lin-stephanie@users.noreply.github.com>
2024-06-11 18:21:20 +0800

    refactor(header) 1. refactor Header, NavTabs into StandardLayout (formerly PostLayout), TabbedLayout 2. modify pages based on refactoring 3. modify BackLink and only implement it in BaseLayout 4. modify rehype-autolink-headings configs 5. move Categorizer

--- plugins/index.ts
+++ plugins/index.ts
@@ -5,15 +5,17 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 export const remarkPlugins: RemarkPlugins = []
 
 export const rehypePlugins: RehypePlugins = [
+  // https://docs.astro.build/en/guides/markdown-content/#heading-ids-and-plugins
   rehypeHeadingIds,
+  // https://github.com/rehypejs/rehype-autolink-headings
   [
     rehypeAutolinkHeadings,
     {
-      behavior: 'prepend', // default
+      behavior: 'append',
       properties: {
         class: 'header-anchor',
-        ariaHidden: 'true', // default
-        tabIndex: -1, // default
+        ariaHidden: 'true',
+        tabIndex: 0,
       },
       content: {
         type: 'text',
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,16 @@
 export type Fn = () => void
+
+export interface NavTabsItem {
+  href: string
+  label: string
+}
+
+export type NavTabsItems = [NavTabsItem, NavTabsItem, ...NavTabsItem[]]
+
+export const items: NavTabsItems = [
+  { href: '/blog', label: 'Blog' },
+  { href: '/talks', label: 'Talks' },
+  { href: '/podcasts', label: 'Podcasts' },
+  { href: '/streams', label: 'Streams' },
+  { href: '/notes', label: 'Notes' },
+]
