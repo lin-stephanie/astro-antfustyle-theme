bce7cee59560fa1359ff0a6ba2d53482e7c5713b
Stephanie Lin <102728558+lin-stephanie@users.noreply.github.com>
2024-09-12 22:19:30 +0800

    feat(rss): support for visitors subscribing to the blog

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,47 @@
+import rss from '@astrojs/rss'
+import { SITE } from '~/config'
+
+export async function GET() {
+  const blogItems = Object.values(
+    import.meta.glob('../content/blog/**/*.{md,mdx}', {
+      eager: true,
+    })
+  )
+
+  const filteredBlogitems = blogItems.filter((item) => !item.frontmatter.draft)
+
+  const sortedBlogItems = filteredBlogitems.sort(
+    (a, b) => new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate)
+  )
+
+  return rss({
+    title: SITE.title,
+    description: SITE.description,
+    site: SITE.website,
+    customData: `
+      <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+      <image>
+        <title>${SITE.title}</title>
+        <url>${SITE.website}/icon-512.png</url>
+        <link>${SITE.website}</link>
+      </image>`,
+
+    items: sortedBlogItems.map((item) => {
+      const slug = item.file
+        .split('/')
+        .pop()
+        .replace(/\.mdx?$/, '')
+
+      return {
+        title: `${item.frontmatter.title}`,
+        link: `/blog/${slug}`,
+        pubDate: item.frontmatter.pubDate,
+        description: item.frontmatter.description,
+        author: SITE.author,
+        content: item.compiledContent(),
+      }
+    }),
+
+    stylesheet: '/rss-styles.xsl',
+  })
+}
