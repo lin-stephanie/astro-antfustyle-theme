f347f4a09359bc5a437fbb901c423f110b363cf3
Stephanie Lin <102728558+lin-stephanie@users.noreply.github.com>
2025-04-28 18:15:45 +0800

    refactor: related to "Changed: TypeScript configuration"
    - See: https://docs.astro.build/en/guides/upgrade-to/v5/#changed-typescript-configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,4 @@
-/// <reference types="astro/client" />
-
-/**
- * @see https://docs.astro.build/en/tutorials/add-content-collections/#create-a-collection-for-your-blog-posts
- */
-/// <reference path="../.astro/types.d.ts" />
-
-/**
- * @see https://docs.astro.build/en/guides/typescript/#infer-getstaticpaths-types
- */
-/// <reference types="astro/astro-jsx" />
-
-/**
- * @description Declares style attributes for UnoCSS's Attributify Mode to enable TypeScript integration.
- * @see https://unocss.dev/presets/attributify#typescript-support-jsx-tsx
- */
+// https://unocss.dev/presets/attributify#typescript-support-jsx-tsx
 import type {
   AttributifyAttributes,
   AttributifyNames,
